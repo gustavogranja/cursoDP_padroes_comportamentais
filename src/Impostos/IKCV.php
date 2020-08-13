cfb52521b2e5d31014f096768bf4ef321d384dd7
gustavogranja <gustavo.cisco235@gmail.com>
2020-08-13 15:09:24 -0300

    Template Method

---
+++ src/Impostos/IKCV.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Alura\DesignPattern\Impostos;
+
+use Alura\DesignPattern\Orcamento;
+
+class IKCV extends ImpostoCom2Aliquotas
+{
+
+    protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
+    {
+        return $orcamento->valor > 300 && $orcamento->quantidadeItnes > 3;
+    }
+
+    protected function calculaTaxaMaxima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.04;
+    }
+
+    protected function calculaTaxaMinima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.025;
+    }
+}
